906780bd62a22c3c4323fba39c1820c59e8a4fbf
nzeenath <nzeenath2002@gmail.com>
2026-07-05 22:02:37 +0530

    3rd commit

---
+++ app.js
@@ -0,0 +1,36 @@
+function btnConformBooking(){
+    let txtFirstName = document.getElementById("txtFirstName");
+    let txtLastName = document.getElementById("txtLastName");
+    let txtPhoneNum = document.getElementById("txtPhoneNum");
+    let txtEmail = document.getElementById("txtEmail");
+    let txtFrom = document.getElementById("txtFrom");
+    let txtTo = document.getElementById("txtTo");
+    let txtAdults = document.getElementById("txtAdults");
+    let txtChildren = document.getElementById("txtChildren");
+    let txtDate = document.getElementById("txtDate");
+    let txtTime = document.getElementById("txtTime");
+
+    // let txtCar = document.getElementById("txtCar");
+    // let txtVan = document.getElementById("txtVan");
+    // let txtBike = document.getElementById("txtBike");
+    // let txtBus = document.getElementById("txtBus");
+  
+    let txtInsuranceYes = document.getElementById("txtInsuranceYes");
+    let txtInsuranceNo = document.getElementById("txtInsuranceNo");
+
+
+    console.log("First Name : "+txtFirstName.value);
+    console.log("Last Name : "+txtLastName.value);
+    console.log("Phone Number : "+txtPhoneNum.value);
+    console.log("Email : "+txtEmail.value);
+    console.log("From : "+txtFrom.value);
+    console.log("To : "+txtTo.value);
+    console.log("Adults : "+txtAdults.value);
+    console.log("Children : "+txtChildren.value);
+    console.log("Date : "+txtDate.value);
+    console.log("Time : "+txtTime.value);
+    // console.log("JourneyType : "+txtJourneyType);
+    console.log("Iclude Insurance - yes  : "+txtInsuranceYes.value);
+    console.log("Iclude Insurance - no   : "+txtInsuranceNo.value);
+    console.log(txtJourneyType);
+}
