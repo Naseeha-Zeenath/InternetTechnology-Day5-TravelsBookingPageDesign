5db2629bc3ec43f2160dcd4795d1a7f85fdcd123
nzeenath <nzeenath2002@gmail.com>
2026-08-11 23:01:13 +0530

    7th commit

--- app.js
+++ app.js
@@ -1,32 +1,25 @@
-function btnConformBooking(){
-    let txtFirstName = document.getElementById("txtFirstName");
-    let txtLastName = document.getElementById("txtLastName");
-    let txtPhoneNum = document.getElementById("txtPhoneNum");
-    let txtEmail = document.getElementById("txtEmail");
-    let txtFrom = document.getElementById("txtFrom");
-    let txtTo = document.getElementById("txtTo");
-    let txtAdults = document.getElementById("txtAdults");
-    let txtChildren = document.getElementById("txtChildren");
-    let txtDate = document.getElementById("txtDate");
-    let txtTime = document.getElementById("txtTime");
 
-  
-    let txtInsuranceYes = document.getElementById("txtInsuranceYes");
-    let txtInsuranceNo = document.getElementById("txtInsuranceNo");
+const bookingList = [];
 
 
-    console.log("First Name : "+txtFirstName.value);
-    console.log("Last Name : "+txtLastName.value);
-    console.log("Phone Number : "+txtPhoneNum.value);
-    console.log("Email : "+txtEmail.value);
-    console.log("From : "+txtFrom.value);
-    console.log("To : "+txtTo.value);
-    console.log("Adults : "+txtAdults.value);
-    console.log("Children : "+txtChildren.value);
-    console.log("Date : "+txtDate.value);
-    console.log("Time : "+txtTime.value);
-    // console.log("JourneyType : "+txtJourneyType);
-    console.log("Iclude Insurance - yes  : "+txtInsuranceYes.value);
-    console.log("Iclude Insurance - no   : "+txtInsuranceNo.value);
-    console.log(txtJourneyType);
+
+
+function btnConformBooking() {
+    let booking = {
+        FirstName: document.getElementById("txtFirstName").value,
+        LastName: document.getElementById("txtLastName").value,
+        PhoneNum: document.getElementById("txtPhoneNum").value,
+        Email: document.getElementById("txtEmail").value,
+        From: document.getElementById("txtFrom").value,
+        To: document.getElementById("txtTo").value,
+        AdultsCount: document.getElementById("txtAdults").value,
+        ChildrenCount: document.getElementById("txtChildren").value,
+        TripDate: document.getElementById("txtDate").value,
+        Time: document.getElementById("txtTime").value
+       
+    }
+    bookingList.push(booking);
+
+    console.log(bookingList);
+
 }
