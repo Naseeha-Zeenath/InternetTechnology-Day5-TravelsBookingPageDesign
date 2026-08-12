762e5de4842e0befe8ec700d8246c966d70a6c4b
nzeenath <nzeenath2002@gmail.com>
2026-08-12 21:25:19 +0530

    9th commit

--- app.js
+++ app.js
@@ -28,5 +28,22 @@ function btnConformBooking() {
 
 
 function btnSearchBooking(){
-    alert("search");
+    let list = JSON.parse(localStorage.getItem());
+
+    let booking = list.find(booking =>{
+        return booking.PhoneNum === document.getElementById("txtPhoneNum").value
+    });
+
+    document.getElementById("txtFirstName").value = booking.FirstName;
+    document.getElementById("txtLastName").value = booking.LastName;
+    document.getElementById("txtPhoneNum").value = booking.PhoneNum;
+    document.getElementById("txtEmail").value = booking.Email;
+    document.getElementById("txtFrom").value = booking.From;
+    document.getElementById("txtTo").value = booking.To;
+    document.getElementById("txtAdults").value = booking.AdultsCount;
+    document.getElementById("txtChildren").value = booking.ChildrenCount;
+    document.getElementById("txtDate").value = booking.TripDate;
+    document.getElementById("txtTime").value = booking.Time;
+
+    console.log(booking);
 }
