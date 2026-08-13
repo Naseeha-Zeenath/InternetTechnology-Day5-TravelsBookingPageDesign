a5066a1de61bf1e6f26c4e5bca9f3817f86dadfb
nzeenath <nzeenath2002@gmail.com>
2026-08-13 18:29:14 +0530

    11th commit

--- app.js
+++ app.js
@@ -2,8 +2,6 @@
 const bookingList = JSON.parse(localStorage.getItem("bookingList"));
 
 
-
-
 function btnConformBooking() {
     let booking = {
         FirstName: document.getElementById("txtFirstName").value,
@@ -16,28 +14,30 @@ function btnConformBooking() {
         ChildrenCount: document.getElementById("txtChildren").value,
         TripDate: document.getElementById("txtDate").value,
         Time: document.getElementById("txtTime").value
-       
+
     }
     bookingList.push(booking);
 
-    console.log(bookingList);
+    console.log(booking);
 
-    localStorage.setItem("bookingList",JSON.stringify(booking));
+    localStorage.setItem("bookingList", JSON.stringify(bookingList));
+
+    btnLoadBookingTable();
 
 }
 
 
-function btnSearchBooking(){
+function btnSearchBooking() {
 
-       let bookingList = JSON.parse(localStorage.getItem("bookingList"));
+    let bookingList = JSON.parse(localStorage.getItem("bookingList"));
 
 
     let booking = bookingList.find(booking => {
-        return booking.FirstName == document.getElementById("txtFirstName").value
+        return booking.FirstName === document.getElementById("txtFirstName").value
     })
 
 
-   
+
 
     document.getElementById("txtFirstName").value = booking.FirstName;
     document.getElementById("txtLastName").value = booking.LastName;
@@ -53,17 +53,61 @@ function btnSearchBooking(){
     console.log(booking);
 }
 
-function btnDeleteBooking(){
-    let bookingList =  JSON.parse(localStorage.getItem("bookingList"));
+function btnDeleteBooking() {
+    let bookingList = JSON.parse(localStorage.getItem("bookingList"));
 
-    let bookingIndex =  bookingList.findIndex(booking =>{
-        return booking.PhoneNum === document.getElementById("txtFirstName").value
+    let bookingIndex = bookingList.findIndex(booking => {
+        return booking.PhoneNum === document.getElementById("txtPhoneNum").value
     })
 
 
-    bookingList.splice(index,1);
+    bookingList.splice(index, 1);
 
     localStorage.setItem("bookingList", JSON.stringify(bookingList));
 
     console.log(bookingList);
+}
+
+
+
+function btnLoadBookingTable() {
+    alert("ok");
+    let bookingList = JSON.parse(localStorage.getItem("bookingList"));
+
+    let body = `
+    <tr style="background-color: cadetblue;">
+            <th>FirstName</th>
+            <th>LastName</th>
+            <th>PhoneNum</th>
+            <th>Email</th>
+            <th>From</th>
+            <th>To</th>
+            <th>AdultsCount</th>
+            <th>ChildrenCount</th>
+            <th>TripDate</th>
+            <th>Time</th>
+        </tr>    
+    `;
+
+    bookingList.forEach(element => {
+        body += `
+       
+             <tr>
+                 <td>${element.FirstName}</td>
+                 <td>${element.LastName}</td>
+                 <td>${element.PhoneNum}</td>
+                 <td>${element.Email}</td>
+                 <td>${element.From}</td>
+                 <td>${element.To}</td>
+                 <td>${element.AdultsCount}</td>
+                 <td>${element.ChildrenCount}</td>
+                 <td>${element.TripDate}</td>
+                 <td>${element.Time}</td>
+             </tr>
+         `
+    })
+
+    document.getElementById("bookingTable").innerHTML = body;
+
+    console.log(body);
 }
